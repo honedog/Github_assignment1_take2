c572c58355f0ad88378900face8b75b2eca072d6
Austin Hone <austinsladehone13@gmail.com>
2024-02-26 21:10:51 -0700

    fixing again

--- streamlit_template/main_page.py
+++ streamlit_template/main_page.py
@@ -6,4 +6,4 @@ st.sidebar.markdown("# Main Page")
 st.write("Click on a page to see racer or kart stats")
 
 link = '[To my Github Pages Site](https://github.com/honedog/Github_assignment1_take2.git)'
-st.markdown(link, unsafe_all_html=True)
+st.markdown(link, unsafe_allow_html=True)
